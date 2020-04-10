ec04ec02a19c6f01fee50769d8d0389ed02cc201
Andrew Senapatiratne <andysenapatiratne@gmail.com>
2020-04-10 13:05:59 -0500

    Fixed naming conventions

--- js/graphupdater.js
+++ js/graphupdater.js
@@ -3,16 +3,16 @@ graph_box = document.getElementById('Graph');
 type_selector.addEventListener('change', () => {
 
   if(type_selector.value =="smooth"){
-    graph_box.src = "images/graphs/smooth_graph.png"
+    graph_box.src = "images/graphs/smoothgraph.png"
   }
   if(type_selector.value =="point"){
-    graph_box.src = "images/graphs/point_graph.jpg"
+    graph_box.src = "images/graphs/pointgraph.jpg"
   }
   if(type_selector.value =="line"){
-    graph_box.src = "images/graphs/line_graph.png"
+    graph_box.src = "images/graphs/linegraph.png"
   }
   if(type_selector.value =="step"){
-    graph_box.src = "images/graphs/step_graph.png"
+    graph_box.src = "images/graphs/stepgraph.png"
   }
 
 });
--- js/slideshowscript.js
+++ js/slideshowscript.js
@@ -3,17 +3,17 @@ var images = [];
 var images_desc = [];
 var pause_time = 3000;
 
-images[0] = 'images/slideshow/image_1.jpg';
+images[0] = 'images/slideshow/image1.jpg';
 images_desc[0] = 'A team photo from MinniMUDAC 2019';
-images[1] = 'images/slideshow/image_5.jpg';
+images[1] = 'images/slideshow/image5.jpg';
 images_desc[1] = 'Data Presentation at MUDAC 2019';
-images[2] = 'images/slideshow/image_3.jpg';
+images[2] = 'images/slideshow/image3.jpg';
 images_desc[2] = 'A team photo from MinniMUDAC 2019';
-images[3] = 'images/slideshow/image_4.jpg';
+images[3] = 'images/slideshow/image4.jpg';
 images_desc[3] = 'A full group photo from MinniMUDAC 2019';
-images[4] = 'images/slideshow/image_2.jpg';
+images[4] = 'images/slideshow/image2.jpg';
 images_desc[4] = 'A team photo from MinniMUDAC 2019';
-images[5] = 'images/slideshow/image_6.jpg';
+images[5] = 'images/slideshow/image6.jpg';
 images_desc[5] = 'Winning UWEC team at MUDAC 2019';
 
 
